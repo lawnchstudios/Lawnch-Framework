3ed00a60869a5be1b605ddb30881ff57c0c4df93
lawnchstudios <andrew.baker@lawnchstudios.com>
2017-06-06 15:53:03 +0000

    Base to .01 Lawnch Framework

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Lawnch Framework</title>
+        <link rel="stylesheet" href="./bower_components/normalize-css/normalize.css" type="text/css" />
+        
+        <link rel="stylesheet" href="./lib/core/css/main.css" type="text/css" />
+        <link rel="stylesheet" href="./bower_components/font-awesome/css/font-awesome.css" type="text/css" />
+        
+        <script type="text/javascript" src="./bower_components/jquery/dist/jquery.js"></script>
+        
+    </head>
+    
+    <!-- #BEGIN SECTION BODY# -->
+    <body class="lawnch">
+        
+        <!-- #BEGIN SECTION NAVBAR# -->
+        <div class="navbar-top">
+            
+            <div class="one-third-column">
+                <div class="brand">
+                    
+                    <img class="brand-logo" src="./lib/core/images/brand-logo.png"></img>
+                    
+                    <h1 class="brand-text">Lawnch Framework</h1>
+                    
+                </div>
+            </div>
+            
+            <div class="one-third-column">
+                
+            </div>
+            
+            <div class="one-third-column">
+                
+                
+                <div class="navigation-button">
+                    <a id="navigation-button-trigger"  href="#"><h2>Menu <i id="menu-icon" class="fa fa-list"></i></h2></a>
+                </div>
+                
+                <ul id="navigation-grid" class="navigation-grid">
+                    
+                    <li class='navigation-item'><h2><a href="">Item One</a></h2></li>
+                    <li class='navigation-item'><h2><a href="">Item Two</a></h2></li>
+                    <li class='navigation-item'><h2><a href="">Item Three</a></h2></li>
+                    
+                    <li class="navigation-item account-panel"><h2><a href="">Account</a></h2>
+                        <ul class="sub-navigation-grid" >
+                            <li class="navigation-item"><h2><a href="">Sign Up</a></h2></li>
+                            <li class="navigation-item"><h2><a href="">Sign In</a></h2></li>
+                            <li class="navigation-item"><h2><a href="">My Account</a></h2></li>
+                        </ul>
+                    </li>
+                    
+                </ul>
+                
+            </div>
+            
+        </div>
+        <!-- #END SECTION NAVBAR# -->
+        
+        
+        <!-- #BEGIN SECTION MAIN# -->
+        <main class="main">
+            
+            <div class="page-wrapper">
+               
+                
+            </div>
+            
+        </main>
+        
+        
+        <div id="night-shade">
+            
+            
+        </div>
+        <!-- #END SECTION MAIN# -->
+        <script type="text/javascript" src="./lib/core/js/lawnch.js"></script>
+        <script type="text/javascript" src="./lib/core/js/nightshade.js"></script>
+    </body>
+</html>
+<!-- #END SECTION BODY# -->
